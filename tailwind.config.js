88c9f3b7c7b74d38f9e52b72b3a44e62e775a83d
luisgcodes <lcienfuegos95@gmail.com>
2022-03-29 19:51:51 -0700

    styles nav

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ["./Develop/js/air-quality.js", "./Develop/js/map.js", "./Develop/js/script.js", "./index.html", "./main.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
