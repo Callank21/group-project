de9deba3ec25027245520695118474f9532e6627
luisgcodes <lcienfuegos95@gmail.com>
2022-03-29 17:40:06 -0700

    adjusting tailwind files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: [],
+  content: ["./Develop/js/air-quality.js", "./Develop/js/map.js", "./Develop/js/script.js", "./index.html", "./main.html"],
   theme: {
     extend: {},
   },
