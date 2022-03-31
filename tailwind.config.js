92c7540fe1ad846aadb6299c1518bb6ec9a283a2
luisgcodes <lcienfuegos95@gmail.com>
2022-03-30 18:55:08 -0700

    centered input text

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  content: ["./Develop/js/air-quality.js", "./Develop/js/map.js", "./Develop/js/script.js", "./index.html", "./main.html"],
+  content: ["./Develop/js/air-quality.js", "./Develop/js/map.js", "./Develop/js/script.js", "./index.html", "./main.html", "./Develop/imgs"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        bhHero:"url('./Develop/imgs/ezgif.com-gif-maker.gif')"
+      },
+
+    },
   },
   plugins: [],
 }
