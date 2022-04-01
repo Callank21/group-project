7fe9c23f18096cd5e191fe7cba695bd42c1592c1
Shaun Kiszonas <smkiszonas@gmail.com>
2022-03-31 17:52:26 -0700

    fixed appending bug

--- Develop/js/air-quality.js
+++ Develop/js/air-quality.js
@@ -1,21 +1,24 @@
-var aqiDef = ["Good", "Fair", "Moderate", "Poor", "Very Poor"];
+var aqiDef = ["good", "fair", "moderate", "poor", "very poor"];
 
 var dataEl = document.querySelector('#test-data');
 var componentsEl = dataEl.querySelector('#component-data');
 var submitButton = document.querySelector('#button');
 
+var qualityEl = document.querySelector('#air-quality');
+
 var airQuality = {};
 
 function buttonSubmitHandler(event) {
     event.preventDefault();
-    var lat = 34.12145650683996;
-    var lon = -118.31018306051159;
+    var lat = 33.94974;
+    var lon = -116.955765;
 
     var airQualityVar = getAirQuality(lat, lon);
     console.log(airQualityVar);
 }
 function getAirQuality(lat, lon) {
     var airPollutionUrl = 'http://api.openweathermap.org/data/2.5/air_pollution?lat=' + lat + '&lon=' + lon + '&appid=4ce1081bbd0cd6d45033a1dc8f18bcdf';
+
 
     fetch(airPollutionUrl)
         .then(function (response) {
@@ -37,10 +40,9 @@ function displayData(data) {
         airQuality.quality = (aqiDef[data.list[i].main.aqi - 1]);
         airQuality.components = data.list[i].components;
 
-        var qualityEl = document.createElement('h2');
         qualityEl.textContent = airQuality.quality;
-        var componentEl = document.createElement('li');
-        componentEl.innerHTML =
+
+        var componentHtml =
             "<li><span> CO:</span> " + airQuality.components.co + "</li>" +
             "<li><span> NO:</span> " + airQuality.components.no + "</li>" +
             "<li><span> NO<sub>2</sub>:</span> " + airQuality.components.no2 + "</li>" +
@@ -50,8 +52,7 @@ function displayData(data) {
             "<li><span> PM<sub>10</sub>:</span> " + airQuality.components.pm10 + "</li>" +
             "<li><span> NH<sub>3</sub>:</span> " + airQuality.components.nh3 + "</li>";
 
-        dataEl.prepend(qualityEl);
-        componentsEl.appendChild(componentEl);
+        componentsEl.innerHTML = componentHtml;
     }
 }
 
