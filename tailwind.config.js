af54ff4cbdd0e010429f6e55d16c3139e5823be7
Sal <salvador.banuelos.424@gmail.com>
2022-04-02 21:20:48 -0700

    adding layout

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./Develop/js/air-quality.js", "./Develop/js/map.js", "./Develop/js/script.js", "./index.html", "./main.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
