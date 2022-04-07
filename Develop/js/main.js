217ae8fda2795f2cfb95f59afb5dfd19548325fe
Callank21 <95372216+Callank21@users.noreply.github.com>
2022-04-07 01:48:20 -0700

    got the history working

--- Develop/js/main.js
+++ Develop/js/main.js
@@ -3,10 +3,11 @@ var queryMap = document.getElementById("queryMap");
 var searchHistory = document.getElementById("searchHistory");
 
 var previousSearches = [];
+console.log(previousSearches.length);
 
 function searchQuery(query) {
-    /*previousSearches.splice(0,0,query); this calls the history tab whenever a search query is input
-    createHistory(previousSearches); */
+    previousSearches.splice(0,0,query); //this calls the history tab whenever a search query is input
+    createHistory(previousSearches);
     query.replace(' ', '%20');
     var location = `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?proximity=ip&types=place%2Cpostcode%2Caddress&access_token=pk.eyJ1IjoiY2FsbGFuazIxIiwiYSI6ImNsMWJiZnA0cjJyMG0zam4xMTF6MHdndmYifQ.lZAx_bYFPQmZDlkT3b8Daw`;
     var lat;
@@ -25,24 +26,35 @@ function searchQuery(query) {
         })
 }
 
-/*function createHistory(previousSearches) { this creates the button and applies the previous search result to the button's textContent whenever a result is generated through searchQuery()
+function createHistory(previousSearches) { //this creates the button and applies the previous search result to the button's textContent whenever a result is generated through searchQuery()
     var searchHistory = document.getElementById("searchHistory");
     deleteHistory();
     localStorage.setItem("generalHistory", JSON.stringify(previousSearches));
-    for(var i =0; i < previousSearches.length; i++) {
-        var button = document.createElement("button");
-        searchHistory.appendChild(button);
-        button.textContent = previousSearches[i];
+    console.log(previousSearches.length);
+    if (previousSearches.length < 6) {
+        for(var i =0; i < previousSearches.length || 0; i++) {
+            var button = document.createElement("button");
+            searchHistory.appendChild(button);
+            button.textContent = previousSearches[i];
+        }
     }
-}*/
+    else if (previousSearches.length >= 6) {
+        previousSearches.length = 5;
+        for(var i =0; i < previousSearches.length || 0; i++) {
+            var button = document.createElement("button");
+            searchHistory.appendChild(button);
+            button.textContent = previousSearches[i];
+        }
+    }
+}
 
-/*function deleteHistory() { this deletes the history tab whenever a new result is added
+function deleteHistory() { //this deletes the history tab whenever a new result is added
     var searchHistory = document.getElementById("searchHistory");
     for (var i = searchHistory.getElementsByTagName('*').length; i > 0; i--) {
         console.log(searchHistory.getElementsByTagName('*').length);
         searchHistory.removeChild(searchHistory.firstChild);
     }
-}*/
+}
 
 function setUpMap(lat, lon) {
     mapboxgl.accessToken = 'pk.eyJ1IjoiY2FsbGFuazIxIiwiYSI6ImNsMWJiZnA0cjJyMG0zam4xMTF6MHdndmYifQ.lZAx_bYFPQmZDlkT3b8Daw';
@@ -77,15 +89,25 @@ if(queryMap) {
     });
 }
 
-/*searchHistory.addEventListener("click", function(event) { this listens to the history div for when it its clicked
+searchHistory.addEventListener("click", function(event) { //this listens to the history div for when it its clicked
     searchQuery(event.target.textContent);
-})*/
+})
 
 var firstSearch = JSON.parse(localStorage.getItem("firstSearch"));
-/*var historySearch = JSON.parse(localStorage.getItem("generalHistory")); this generates the history underneath the search bar when the screen loads
-for(var i = 0; i < historySearch.length; i++){
-    searchQuery(historySearch[i]);
-} */
+var historySearch = [];
+historySearch = previousSearches.splice(0,0,JSON.parse(localStorage.getItem("generalHistory"))); //this generates the history underneath the search bar when the screen loads
+
+if (historySearch.length < 6) {
+    for(var i = 0; i < historySearch.length || 0; i++){
+        searchQuery(historySearch[i]);
+    }
+}
+else if (historySearch.length >= 6) {
+    historySearch.length = 5;
+    for(var i = 0; i < historySearch.length || 0; i++){
+        searchQuery(historySearch[i]);
+    }
+} 
 searchQuery(firstSearch);
 
 // Start of Air Pollution API
@@ -117,23 +139,25 @@ function getAirQuality(lat, lon) {
 }
 
 function displayData(data) {
-    for (let i = 0; i < data.list.length; i++) {
-        airQuality.quality = (aqiDef[data.list[i].main.aqi - 1]);
-        airQuality.components = data.list[i].components;
-
-        qualityEl.textContent = airQuality.quality;
-
-        var componentHtml =
-            "<li><span> CO:</span> " + airQuality.components.co + "</li>" +
-            "<li><span> NO:</span> " + airQuality.components.no + "</li>" +
-            "<li><span> NO<sub>2</sub>:</span> " + airQuality.components.no2 + "</li>" +
-            "<li><span> O<sub>3</sub>:</span> " + airQuality.components.o3 + "</li>" +
-            "<li><span> SO<sub>2</sub>:</span> " + airQuality.components.so2 + "</li>" +
-            "<li><span> PM<sub>2.5</sub>:</span> " + airQuality.components.pm2_5 + "</li>" +
-            "<li><span> PM<sub>10</sub>:</span> " + airQuality.components.pm10 + "</li>" +
-            "<li><span> NH<sub>3</sub>:</span> " + airQuality.components.nh3 + "</li>";
-
-        componentsEl.innerHTML = componentHtml;
+    if(data.list.length){
+        for (let i = 0; i < data.list.length; i++) {
+            airQuality.quality = (aqiDef[data.list[i].main.aqi - 1]);
+            airQuality.components = data.list[i].components;
+    
+            qualityEl.textContent = airQuality.quality;
+    
+            var componentHtml =
+                "<li><span> CO:</span> " + airQuality.components.co + "</li>" +
+                "<li><span> NO:</span> " + airQuality.components.no + "</li>" +
+                "<li><span> NO<sub>2</sub>:</span> " + airQuality.components.no2 + "</li>" +
+                "<li><span> O<sub>3</sub>:</span> " + airQuality.components.o3 + "</li>" +
+                "<li><span> SO<sub>2</sub>:</span> " + airQuality.components.so2 + "</li>" +
+                "<li><span> PM<sub>2.5</sub>:</span> " + airQuality.components.pm2_5 + "</li>" +
+                "<li><span> PM<sub>10</sub>:</span> " + airQuality.components.pm10 + "</li>" +
+                "<li><span> NH<sub>3</sub>:</span> " + airQuality.components.nh3 + "</li>";
+    
+            componentsEl.innerHTML = componentHtml;
+        }
     }
 }
 
